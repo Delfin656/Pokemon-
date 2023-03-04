db7c60fa4f1e3f1cccb29b304539a20af2304a2a
Delfin656 <juandelfin656@gmail.com>
2023-03-04 10:22:07 +0000

    agregue diseño al navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,68 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Pokemon from "../../img/pokemon-logo-transparente.png";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-danger pt-0 pb-0">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand ms-5">
+          <img src={Pokemon} alt="" width="100" height="80" className="img-fluid" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-between"
+          id="navbarSupportedContent"
+        >
+          <div></div>
+          <ul className="navbar-nav mb-2 mb-lg-0"></ul>
+
+          <div>
+            <Link
+              to="/login"
+              style={{
+                color: "Black",
+                textDecoration: "none",
+              }}
+            >
+              <button
+                className="button-login btn btn-light me-2 p-2 "
+                type="submit"
+                style={{
+                  borderRadius: "8px",
+                }}
+              >
+                Iniciar Sesión
+              </button>
+            </Link>
+
+            <Link
+              to="/registro"
+              style={{ color: "Black", textDecoration: "none" }}
+            >
+              <button
+                className="button-registro btn btn-light p-2 me-3"
+                type="submit"
+                style={{
+                  borderRadius: "8px",
+                }}
+              >
+                Registrate
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
