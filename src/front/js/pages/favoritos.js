ab90c53338ae91655203152d5d02d0358162f64e
Delfin656 <juandelfin656@gmail.com>
2023-03-07 00:10:33 +0000

    update

--- src/front/js/pages/favoritos.js
+++ src/front/js/pages/favoritos.js
@@ -1,16 +1,18 @@
 import React from "react";
 // import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-
-export const Favoritos = () =>{
-    
-
-
-    return(
-
-        <div>
-            <h1>Hola mundo estas en Favoritos</h1>
-            
-        </div>
-    )
-}
+export const Favoritos = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <h1>Hola mundo estas en Favoritos</h1>
+      <div className="btn-backPokemones d-flex justify-content-center">
+        <Link to="/pokemones">
+          <button type="button" className="btn btn-secondary">
+            Back to pokemones
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
